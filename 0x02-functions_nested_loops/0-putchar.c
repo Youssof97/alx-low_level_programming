c2db19091f94f26d4cf75414f4611133283b8904
Youssof97 <yousefabdalrhman97@gmail.com>
2023-09-12 11:45:35 -0700

    first task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int _putchar(char c) {
+            return write(1, &c, 1);
+}
+
+int main() {
+            _putchar('_');
+            _putchar('p');
+            _putchar('u');
+             _putchar('t');
+            _putchar('c');
+             _putchar('h');
+             _putchar('a');
+            _putchar('r');
+            _putchar('\n');
+         return 0;
+}
+

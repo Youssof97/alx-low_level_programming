a936935dbbb1b849e86db3d96a0d9de7cbca0813
Youssof97 <yousefabdalrhman97@gmail.com>
2023-09-13 00:25:28 +0300

    functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,22 +2,22 @@
 #include "main.h"
 
 /**
- * main - entry point of the program 
+ * main - entry point of the program
  *
- * Return: Always 0 (sucess_.
+ * Return: Always 0 (sucess)_.
  */
 
 int main (void);
 {
-	char *word ="_putchar\n");
+	char *word = "_putchar\n";
 	int i ;
 
-	for (i = 0 ; i < 9 ; i++);
+	for (i = 0; i < 9; i++)
 	{
 		_putchar(word[i]);
 
 	}
-	
+
 	return(0)
 
 }

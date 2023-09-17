f8674398f301a3f47222f936eb4baee8594c3a39
Youssof97 <yousefabdalrhman97@gmail.com>
2023-09-17 06:47:48 -0700

    last task

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,24 +1,91 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point of the program
- * a program to calculate the sum of multiples of 3 or 5 under 1024
+ * count_zeros - count trailing zeros in integer n
+ * @n: the number to count it's trailing zeros
  *
- * Return: 0 (success)
+ * Return: the count of zeros
  */
 
-int main(void)
+int count_zeros(int n)
 {
-	int i;
-	int sum = 0;
+	int tmp = n;
+	int count = 0;
 
-	for (i = 0; i < 1024; i++)
+	while (tmp % 10 == 0)
 	{
-	if (i % 3 == 0 || i % 5 == 0
-	sum += i;		
+		if (tmp == 0)
+			break;
+		count++;
+		tmp /= 10;
 	}
 
-        printf("%i\n", sum);
+	return (count);
+}
 
-	return (0);
+/**
+ * print_digits - prints all digits of number n
+ * @n: the number that will be printed
+ */
+
+void print_digits(int n)
+{
+	int tmp = n;
+	int digits = 0;
+	int zero_count = count_zeros(n);
+	int i;
+
+	while (tmp > 0)
+	{
+		digits = digits * 10 + (tmp % 10);
+		tmp /= 10;
+	}
+	do {
+		_putchar(digits % 10 + '0');
+		digits /= 10;
+	} while (digits > 0);
+
+	for (i = 0; i < zero_count; i++)
+	{
+		_putchar('0');
+	}
+
+
+}
+
+/**
+ * print_times_table - prints times table from 0 to n
+ * @n: the number to print times table to
+ */
+
+void print_times_table(int n)
+{
+	int i;
+	int j;
+
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			print_digits(i * j);
+			if (j == n)
+			{
+				break;
+			}
+			_putchar(',');
+			_putchar(' ');
+			if ((i * (j + 1)) < 10)
+				_putchar(' ');
+			if ((i * (j + 1)) < 100)
+				_putchar(' ');
+
+		}
+		_putchar('\n');
+	}
+
 }

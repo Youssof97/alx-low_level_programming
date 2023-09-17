b40aa1e9a32988cf86765926d24435bfd7c4d868
Youssof97 <yousefabdalrhman97@gmail.com>
2023-09-17 06:49:41 -0700

    last task

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,91 +1,24 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * count_zeros - count trailing zeros in integer n
- * @n: the number to count it's trailing zeros
+ * main - Entry point of the program
+ * a program to calculate the sum of multiples of 3 or 5 under 1024
  *
- * Return: the count of zeros
+ * Return: 0 (success)
  */
 
-int count_zeros(int n)
-{
-	int tmp = n;
-	int count = 0;
-
-	while (tmp % 10 == 0)
-	{
-		if (tmp == 0)
-			break;
-		count++;
-		tmp /= 10;
-	}
-
-	return (count);
-}
-
-/**
- * print_digits - prints all digits of number n
- * @n: the number that will be printed
- */
-
-void print_digits(int n)
-{
-	int tmp = n;
-	int digits = 0;
-	int zero_count = count_zeros(n);
-	int i;
-
-	while (tmp > 0)
-	{
-		digits = digits * 10 + (tmp % 10);
-		tmp /= 10;
-	}
-	do {
-		_putchar(digits % 10 + '0');
-		digits /= 10;
-	} while (digits > 0);
-
-	for (i = 0; i < zero_count; i++)
-	{
-		_putchar('0');
-	}
-
-
-}
-
-/**
- * print_times_table - prints times table from 0 to n
- * @n: the number to print times table to
- */
-
-void print_times_table(int n)
+int main(void)
 {
 	int i;
-	int j;
+	int sum = 0;
 
-	if (n > 15 || n < 0)
+	for (i = 0; i < 1024; i++)
 	{
-		return;
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
 	}
 
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			print_digits(i * j);
-			if (j == n)
-			{
-				break;
-			}
-			_putchar(',');
-			_putchar(' ');
-			if ((i * (j + 1)) < 10)
-				_putchar(' ');
-			if ((i * (j + 1)) < 100)
-				_putchar(' ');
+	printf("%i\n", sum);
 
-		}
-		_putchar('\n');
-	}
-
+	return (0);
 }

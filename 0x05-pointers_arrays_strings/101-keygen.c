6997c076a80cee7c289f219044ecb0cd9dea7b14
Youssof97 <yousefabdalrhman97@gmail.com>
2023-09-18 19:22:16 -0700

    advance task

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - generates a random password based on current time
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long t = time(NULL);
+	int r;
+	int i;
+	char printable_chars[] = "abcdefgmnopqrstusfdfdafsdfgdsgfdsfadafdassdfWXYZ0123456789/,.-+=~`<>:";
+
+	srand((unsigned int) t);
+	for (i = 0; i < 3; i++)
+	{
+		r = rand() % (sizeof(printable_chars) - 1);
+		printf("%c", printable_chars[r]);
+	}
+
+	return (0);
+}

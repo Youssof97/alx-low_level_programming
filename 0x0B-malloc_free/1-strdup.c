fd307b449b7d3be5bf5417351953984996104534
Youssof97 <yousefabdalrhman97@gmail.com>
2023-10-04 16:31:56 -0700

    malloc task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string in the heap
+ * @str: the string to be duplicated
+ * Return: a pointer to the string
+ */
+
+char *_strdup(char *str)
+{
+	int size;
+	int i;
+	char *duplicate;
+
+	if (str == NULL)
+		return (NULL);
+
+	size = strlen(str);
+	duplicate = malloc(sizeof(char) * size + 1);
+
+	if (duplicate != NULL)
+	{
+		for (i = 0; i <= size; i++)
+		{
+			duplicate[i] = str[i];
+		}
+		return (duplicate);
+	}
+
+	return (NULL);
+}
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - allocates a two dimensional array of integers in heap
+ * @width: the width of the grid
+ * @height: the height of the grid
+ * Return: a pointer to the grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int row;
+	int col;
+	int **grid;
+	int i;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = (int **) malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+		return (NULL);
+	for (row = 0; row < height; row++)
+	{
+		grid[row] = (int *) malloc(sizeof(int) * width);
+		if (grid[row] == NULL)
+		{
+			for (i = 0; i < row; i++)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
+	}
+
+	for (row = 0; row < height; row++)
+	{
+		for (col = 0; col < width; col++)
+		{
+			grid[row][col] = 0;
+		}
+	}
+
+	return (grid);
+}
